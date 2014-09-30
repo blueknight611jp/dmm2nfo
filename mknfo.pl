54b6f0635d8a23d391283573d5debc4eb245188c
nob <blueknight611jp@yahoo.co.jp>
2014-10-01 07:34:51 +0900

    use Web::Scraper

--- mknfo.pl
+++ mknfo.pl
@@ -1,5 +1,7 @@
 use Web::Scraper;
+use Encode;
 use URI;
+use DDP;
 
 if(!@ARGV) {
 	print "Usage: mknfo moviename\n";
@@ -22,5 +24,19 @@ print "searching $searchWord\n";
 
 # http://www.dmm.co.jp/search/=/searchstr=ADN-033
 
-my $searchUrl = "http://www.dmm.co.jp/search/=/searchstr=$searchWord";
+my $searchUrl = "http://www.dmm.co.jp/search/=/searchstr=$searchWord/view=text/";
 print "$searchUrl\n";
+
+
+my $uri = URI->new("$searchUrl");
+my $scraper = scraper {
+     process "/html/body/table/tr/td[2]/div/div[1]/div[2]/div/table/tr[2]/td[1]/p[1]/a",'list' => 'HTML';
+};
+my $res = $scraper->scrape($uri);
+
+my $out = encode('utf-8',$res->{list});
+ 
+print "$out\n";
+
+
+
